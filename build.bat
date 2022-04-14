d226c386f6573fe10aa21e39727b5eb644a82d62
Bailey <Anequit@gmail.com>
2022-04-14 17:42:39 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-pyinstaller --noconfirm --clean -D -F -i ./src/Assets/ICON.ico -n "WST" ./src/main.py
+pyinstaller --noconfirm --clean -D -F -i ./src/Assets/ICON.ico -n "WinOptimizer" ./src/main.py
 
 cd dist
 explorer.exe .
