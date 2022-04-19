0fcadb337dde0bc0f7927840bfcaeb51e1b17300
Bailey <Anequit@gmail.com>
2022-04-19 15:15:29 -0400

    Fixed build.ps1 errors

--- build.ps1
+++ build.ps1
@@ -3,11 +3,11 @@ pyinstaller --noconfirm --clean -D -F -i ./src/Assets/ICON.ico -n "WinOptimizer"
 Remove-Item -R -Force ".\build"
 Remove-Item -R -Force ".\src\__pycache__"
 Remove-Item -Force "WinOptimizer.spec"
-Remove-Item -R -Force ".\bin\*"
+Remove-Item -R -Force ".\bin\"
 
-Rename-Item ".\dist" "bin" 
+Rename-Item ".\dist" ".\bin" 
 
-Set-Location bin
+Set-Location ".\bin"
 
 Get-FileHash -A SHA256 ".\WinOptimizer.exe" | Format-List >> SHA256
 Get-Content SHA256
