efa47f2aba2364a1fc03391ab25f1a760500b7f8
Bailey <Anequit@gmail.com>
2022-05-17 17:14:08 -0400

    Remove unnecessary command

--- build.ps1
+++ build.ps1
@@ -1,7 +1,6 @@
 pyinstaller --noconfirm --clean -D -F -i ./src/Assets/ICON.ico -n "WinOptimizer" ./src/main.py
 
 Remove-Item -R -Force ".\build"
-Remove-Item -R -Force ".\src\__pycache__"
 Remove-Item -Force "WinOptimizer.spec"
 Remove-Item -R -Force ".\bin\"
 
