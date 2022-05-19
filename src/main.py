020bbbabfeae2c10e81e3b071ce6c9dacc974880
Bailey <Anequit@gmail.com>
2022-05-19 18:08:30 -0400

    Fix invalid key value

--- src/main.py
+++ src/main.py
@@ -53,11 +53,12 @@ registry_keys = [(r"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AxInstS
                  (r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_MAXCONNECTIONSPERSERVER\explorer.exe", 10, winreg.REG_DWORD),
                  (r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_MAXCONNECTIONSPER1_0SERVER\explorer.exe", 10, winreg.REG_DWORD),
                  (r"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications\GlobalUserDisabled", 1, winreg.REG_DWORD),
-                 (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_Enabled", 0, winreg.REG_DWORD),
+                 (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_Enabled", 1, winreg.REG_DWORD),
                  (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_FSEBehaviorMode", 2, winreg.REG_DWORD),
-                 (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_HonorUserFSEBehaviorMode", 0, winreg.REG_DWORD),
+                 (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_FSEBehavior", 2, winreg.REG_DWORD),
+                 (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_HonorUserFSEBehaviorMode", 1, winreg.REG_DWORD),
                  (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_DXGIHonorFSEWindowsCompatible", 1, winreg.REG_DWORD),
-                 (r"HKEY_CURRENT_USER\System\GameConfigStore\GameDVR_EFSEFeatureFlags", 0, winreg.REG_DWORD)]
+                 (r"HKEY_CURRENT_USER\System\GameConfigStore\`GameDVR_EFSEFeatureFlags", 0, winreg.REG_DWORD)]
 
 cleanup_keys = [(r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Active Setup Temp Folders\StateFlags0000", 2, winreg.REG_DWORD),
                 (r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\BranchCache\StateFlags0000", 2, winreg.REG_DWORD),
