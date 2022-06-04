1588f48430e061255008ffb911bd46996072f589
Bailey <Anequit@gmail.com>
2022-06-04 16:35:50 -0400

    Refactor powershell

--- src/utils/powershell.py
+++ src/utils/powershell.py
@@ -1,17 +1,19 @@
 import subprocess
 
+
 def execute_command(command: str) -> None:
-    if command == None:
+    if command is None:
         raise ValueError("command can't be empty")
 
-    try:
-        subprocess.check_call(["powershell", command], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
-    except:
+    process = subprocess.call(["powershell", command], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+    if process != 0:
         print("  - Failed to run: " + command)
 
+
 def execute_commands(*commands: str) -> None:
-    if commands == None:
+    if commands is None:
         raise ValueError("commands can't be empty")
-    
+
     for command in commands:
         execute_command(command)
