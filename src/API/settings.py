f43c0d4dd97419704b13a40079c54d2a8ba5562f
Bailey <Anequit@gmail.com>
2022-07-01 22:25:59 -0400

    Update settings.py

--- src/API/settings.py
+++ src/API/settings.py
@@ -1,15 +1,8 @@
 from options import Options
-from requests import get
-
 
 class Settings:
     def __init__(self) -> None:
         self.options = Options().__dict__()
-        self.latest = Settings.get_latest_version()
         
     def __dict__(self) -> dict[str]:
-        return dict(options=self.options, latestVersion=self.latest)
-    
-    @staticmethod
-    def get_latest_version() -> str:
-        return get("https://api.github.com/repos/Anequit/win-optimizer/releases/latest").json()['tag_name']
+        return dict(options=self.options)
