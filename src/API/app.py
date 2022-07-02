7f3eef3e2f8aa62c86805c0b084ce658bf8930a4
Bailey <Anequit@gmail.com>
2022-07-01 22:25:55 -0400

    Update app.py

--- src/API/app.py
+++ src/API/app.py
@@ -1,10 +1,14 @@
 from json import dumps
 from fastapi import FastAPI
 from settings import Settings
-
+from version import Version
 
 app = FastAPI()
 
-@app.get("/")
-def root():
+@app.get("/settings")
+def settings():
     return dumps(Settings().__dict__())
+
+@app.get("/version")
+def version():
+    return Version.get_latest_version()
