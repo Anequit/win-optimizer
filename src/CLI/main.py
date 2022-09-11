9a2ff545294642165569e594537e2accf75661d3
Bailey <Anequit@gmail.com>
2022-09-11 16:29:56 -0400

    Update main.py

--- src/CLI/main.py
+++ src/CLI/main.py
@@ -1,16 +1,39 @@
-from utils import system
 from sys import exit
 from menu import *
+from ctypes import windll
+from os import name
+from sys import executable, argv, exit
+
+
+def is_admin() -> bool:
+    "Check for administrator privileges"
+    
+    return windll.Shell32.IsUserAnAdmin()
+
+def elevate_privileges() -> None:
+    "Elevate script to administrator privileges"
+
+    windll.shell32.ShellExecuteW(None, "runas", executable, " ".join(argv), None, 1)
+    exit(0)
+
+def is_windows() -> bool:
+    "Check if OS is Windows"
+    
+    # Check for NT
+    if(name != "nt"):
+        return False
+    
+    return True
 
 def main() -> None:
-    if(system.is_windows() == False):
+    if(is_windows() == False):
         print("This script is only designed to run on Windows operating systems.")
         input("Press any key to close..")
         exit(0)
         
     # FIXME: Make != into ==
-    if(system.is_admin() != False):
-        system.elevate_privileges()
+    if(is_admin() != False):
+        elevate_privileges()
         
     while(True):
         clear_screen()
@@ -30,6 +53,8 @@ def main() -> None:
             continue
         
         resolve_option(section, option)
-
+        
+        
 if __name__ == "__main__":
     main()
+
