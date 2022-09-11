58e9bfd9cecd2d1f1399e0b146df02cc37b66d94
Bailey <Anequit@gmail.com>
2022-09-11 17:10:36 -0400

    Delete registry.py

--- src/CLI/utils/registry.py
+++
@@ -1,9 +0,0 @@
-from utils.powershell import Powershell
-
-
-def backup() -> None:
-    Powershell.execute_command("Enable-ComputerRestore -Drive 'C:\'")
-    Powershell.execute_command("Checkpoint-Computer -Description win-optimizer -RestorePointType MODIFY_SETTINGS")
-
-def restore() -> None:
-    Powershell.execute_command("Rstrui.exe")
