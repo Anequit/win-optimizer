ff82a5d5185819393071d8af6a574de1ce73129d
Bailey <Anequit@gmail.com>
2022-09-10 23:25:21 -0400

    Update powershell.py

--- src/CLI/utils/powershell.py
+++ src/CLI/utils/powershell.py
@@ -1,21 +1,18 @@
 import subprocess
 
 
-class Powershell:
-    @staticmethod
-    def execute_command(command: str) -> None:
-        if command is None:
-            raise ValueError("command can't be empty")
+def execute_command(command: str) -> None:
+    if command is None:
+        raise ValueError("command can't be empty")
 
-        process = subprocess.run(["powershell", command], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True, capture_output=False)
+    process = subprocess.run(f"powershell {command}", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True, capture_output=False)
 
-        if process.returncode != 0:
-            print("  - Failed to run: " + command)
+    if process.returncode != 0:
+        print("  - Failed to run: " + command)
 
-    @staticmethod
-    def execute_commands(commands: list) -> None:
-        if commands is None:
-            raise ValueError("commands can't be empty")
+def execute_commands(commands: list) -> None:
+    if commands is None:
+        raise ValueError("commands can't be empty")
 
-        for command in commands:
-            Powershell.execute_command(command)
+    for command in commands:
+        execute_command(command)
