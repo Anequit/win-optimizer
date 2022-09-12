2e598bc84382a5bf8318804548575082e5dec403
Bailey <Anequit@gmail.com>
2022-09-12 17:22:07 -0400

    added prompt for registry backup

--- src/CLI/options/registry.py
+++ src/CLI/options/registry.py
@@ -1,8 +1,13 @@
 from utils.command import execute_command
+from rich.prompt import Confirm
+
 
 def backup_registry() -> None:
-    execute_command("powershell Enable-ComputerRestore -Drive 'C:\'")
-    execute_command("powershell Checkpoint-Computer -Description win-optimizer -RestorePointType MODIFY_SETTINGS")
+    result = Confirm.ask("Would you like to backup the registry?", default=True, show_default=True)
+    
+    if(result == True):
+        execute_command("powershell Enable-ComputerRestore -Drive 'C:\'")
+        execute_command("powershell Checkpoint-Computer -Description win-optimizer -RestorePointType MODIFY_SETTINGS")
 
 def restore_registry() -> None:
     execute_command("Rstrui.exe")
