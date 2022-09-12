e5ffb31c270342f744ae7999395d468e08dd2230
Bailey <Anequit@gmail.com>
2022-09-12 17:27:04 -0400

    Added skip_prompt to backup_registry

--- src/CLI/options/registry.py
+++ src/CLI/options/registry.py
@@ -2,12 +2,16 @@ from utils.command import execute_command
 from rich.prompt import Confirm
 
 
-def backup_registry() -> None:
-    result = Confirm.ask("Would you like to backup the registry?", default=True, show_default=True)
+def backup_registry(skip_prompt: bool = False) -> None:
+    if(skip_prompt == False):
+        result = Confirm.ask("Would you like to backup the registry?", default=True, show_default=True)
+        
+        # Doesn't want to backup registry
+        if(result == False):
+            return
     
-    if(result == True):
-        execute_command("powershell Enable-ComputerRestore -Drive 'C:\'")
-        execute_command("powershell Checkpoint-Computer -Description win-optimizer -RestorePointType MODIFY_SETTINGS")
+    execute_command("powershell Enable-ComputerRestore -Drive 'C:\'")
+    execute_command("powershell Checkpoint-Computer -Description win-optimizer -RestorePointType MODIFY_SETTINGS")
 
 def restore_registry() -> None:
     execute_command("Rstrui.exe")
