13af9558ecaccb01875fe844430da3144d9f5ad1
Bailey <Anequit@gmail.com>
2022-10-02 20:59:16 -0400

    Fix file handler

--- src/CLI/options/software_activation.py
+++ src/CLI/options/software_activation.py
@@ -15,7 +15,7 @@ def activate_winrar() -> None:
     # check if winrar is installed
     if isdir(r"C:\Program Files\WinRAR"):
         # open key file
-        file = open(r"C:\Program Files\WinRAR\rarreg.key", 'w')
         
-        # write registration key to file
-        file.write(("""RAR registration data\nHardik\nwww.Hardik.live\nUID=448c4a899c6cdc1039c5\n641221225039c585fc5ef8da12ccf689780883109587752a828ff0\n59ae0579fe68942c97d160f361d16f96c8fe03f1f89c66abc25a37\n7777a27ec82f103b3d8e05dcefeaa45c71675ca822242858a1c897\nc57d0b0a3fe7ac36c517b1d2be385dcc726039e5f536439a806c35\n1e180e47e6bf51febac6eaae111343d85015dbd59ba45c71675ca8\n2224285927550547c74c826eade52bbdb578741acc1565af60e326\n6b5e5eaa169647277b533e8c4ac01535547d1dee14411061928023""").strip())
+        with open(r"C:\Program Files\WinRAR\rarreg.key", 'w') as file:        
+            # write registration key to file
+            file.write(("""RAR registration data\nHardik\nwww.Hardik.live\nUID=448c4a899c6cdc1039c5\n641221225039c585fc5ef8da12ccf689780883109587752a828ff0\n59ae0579fe68942c97d160f361d16f96c8fe03f1f89c66abc25a37\n7777a27ec82f103b3d8e05dcefeaa45c71675ca822242858a1c897\nc57d0b0a3fe7ac36c517b1d2be385dcc726039e5f536439a806c35\n1e180e47e6bf51febac6eaae111343d85015dbd59ba45c71675ca8\n2224285927550547c74c826eade52bbdb578741acc1565af60e326\n6b5e5eaa169647277b533e8c4ac01535547d1dee14411061928023""").strip())
